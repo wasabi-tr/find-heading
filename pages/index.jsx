4aa60208faa5166b6fa8bbcad82f6c93266ad330
github takaryu39 <ryuta0takamiya@gmail.com>
2023-02-18 15:10:46 +0900

    first-cimmit

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Layout from '@/components/Layout'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Layout>
+        <a href="">test</a>
+      </Layout>
+    </>
+  )
+}
