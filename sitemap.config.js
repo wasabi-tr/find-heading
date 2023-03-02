5d340f14491c054d34348c82d2e64dab8a6a5618
github takaryu39 <ryuta0takamiya@gmail.com>
2023-03-03 07:29:29 +0900

    XMLサイトマップの修正

--- sitemap.config.js
+++ sitemap.config.js
@@ -2,7 +2,7 @@ const { siteMeta } = require("@/lib/contents")
 
 const { siteUrl } = siteMeta
 module.exports = {
-    siteUrl: process.env.SITE_URL || siteUrl,
+    siteUrl: siteUrl,
     generateRobotsTxt: true, // (optional)
     outDir: './out'
     // ...other options
