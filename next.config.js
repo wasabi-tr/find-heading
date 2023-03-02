4028bc8d1b8ff87451170803d7f71fd1d55fb300
github takaryu39 <ryuta0takamiya@gmail.com>
2023-03-03 08:58:54 +0900

    next.config.jsが消えていたのを復帰

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images:{
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.microcms-assets.io',
+          port: '',
+          pathname: '/assets/**',
+        },
+      ],
+    },
+  }
+  
+  module.exports = nextConfig
