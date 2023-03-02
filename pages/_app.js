a8c44ef40f9ce7e260b95ef0d1fbd683aa3bdcd5
github takaryu39 <ryuta0takamiya@gmail.com>
2023-03-03 06:46:01 +0900

    アナリティクスタグ設置

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,48 @@ import '@unocss/reset/tailwind.css'
 import '@/styles/globals.scss'
 import Layout from '@/components/Layout'
 import SideMenu from '@/components/SideMenu'
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { GA_MEASUREMENT_ID, pageview } from '@/components/Gtag';
+
 
 export default function App({ Component, pageProps }) {
-
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
   return (
-    <Layout>
-      <SideMenu></SideMenu>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
+
+      <Layout>
+        <SideMenu></SideMenu>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 
 }
