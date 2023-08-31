c1d3d19dc89a22105bd50c5ad472181b3d1fb110
github takaryu39 <ryuta0takamiya@gmail.com>
2023-09-01 08:35:09 +0900

    カテゴリー出力をSSGに

--- components/SideMenu.jsx
+++ components/SideMenu.jsx
@@ -1,71 +1,75 @@
 import styles from '@/styles/side-menu.module.scss'
-import { useEffect, useRef, useState } from 'react';
-import Categories from './Categories';
-import GlobalNav from './GlobalNav';
+import { useEffect, useRef, useState } from 'react'
+import Categories from './Categories'
+import GlobalNav from './GlobalNav'
 function SideMenu() {
+  const [isHover, setIsHover] = useState(false)
+  const [ariaExpanded, setAriaExpanded] = useState(false)
 
-    const [isHover, setIsHover] = useState(false);
-    const [ariaExpanded, setAriaExpanded] = useState(false)
-    
-    const clickOpenSideMenu = () => {
-        setIsHover(!isHover)
-        setAriaExpanded(!ariaExpanded)
+  const clickOpenSideMenu = () => {
+    setIsHover(!isHover)
+    setAriaExpanded(!ariaExpanded)
+  }
+  useEffect(() => {
+    const body = document.querySelector('body')
+    if (isHover) {
+      body.style.overflow = 'hidden'
+      body.style.height = '100%'
+    } else {
+      body.style.overflow = ''
+      body.style.height = ''
     }
-    useEffect(() => {
-        const body = document.querySelector('body');
-        if (isHover) {
-            body.style.overflow = 'hidden';
-            body.style.height = '100%';
-        } else {
-            body.style.overflow = '';
-            body.style.height = '';
-        }
-    }, [isHover]);
+  }, [isHover])
 
-    const buttonRef = useRef(null)
-    const handleFocus = () => {
-        buttonRef.current.focus()
-    }
-    return (
-        <>
-            <div className={isHover ? styles.open : styles.close}>
-                <div className={styles.buttonWrap}>
-                    <button
-                        className={"flex flex-col justify-center items-center w-full h-full"}
-                        onKeyDown={(event) => {
-                            event.code === 'Escape' ? clickOpenSideMenu() : null
-                        }}
-                        onClick={clickOpenSideMenu}
-                        ref={buttonRef}
-                        aria-label="サイドメニュー"
-                        aria-controls="side-menu"
-                        aria-expanded={ariaExpanded}
-                    >
-                        <div className={styles.hamburger}  >
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                        <div className={styles.button}>カテゴリー</div>
-                    </button>
-                </div>
-                <nav id="side-menu" className={styles.menu_wrap} aria-label="サイドメニュー">
-                    <Categories
+  const buttonRef = useRef(null)
+  const handleFocus = () => {
+    buttonRef.current.focus()
+  }
+  return (
+    <>
+      <div className={isHover ? styles.open : styles.close}>
+        <div className={styles.buttonWrap}>
+          <button
+            className={
+              'flex flex-col justify-center items-center w-full h-full'
+            }
+            onKeyDown={(event) => {
+              event.code === 'Escape' ? clickOpenSideMenu() : null
+            }}
+            onClick={clickOpenSideMenu}
+            ref={buttonRef}
+            aria-label="サイドメニュー"
+            aria-controls="side-menu"
+            aria-expanded={ariaExpanded}
+          >
+            <div className={styles.hamburger}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className={styles.button}>カテゴリー</div>
+          </button>
+        </div>
+        <nav
+          id="side-menu"
+          className={styles.menu_wrap}
+          aria-label="サイドメニュー"
+        >
+          {/* <Categories
                         closeMenu={() => setIsHover(false)}
                         ariaExpanded={() => setAriaExpanded(false)}
                         lastCategoryFocus={() => setLastCategoryFocus(true)}
-                    />
-                    <GlobalNav
-                        isPcShow={true}
-                        closeMenu={() => setIsHover(false)}
-                        ariaExpanded={() => setAriaExpanded(false)}
-                    />
-                    <div onFocus={handleFocus} tabIndex={0}></div>
-                </nav>
-            </div>
-
-        </>
-    );
+                    /> */}
+          <GlobalNav
+            isPcShow={true}
+            closeMenu={() => setIsHover(false)}
+            ariaExpanded={() => setAriaExpanded(false)}
+          />
+          <div onFocus={handleFocus} tabIndex={0}></div>
+        </nav>
+      </div>
+    </>
+  )
 }
 
-export default SideMenu;
+export default SideMenu
--- pages/category/index.jsx
+++ pages/category/index.jsx
@@ -1,32 +1,56 @@
-import Breadcrumb from "@/components/Breadcrumb";
-import Categories from "@/components/Categories";
-import Meta from "@/components/Meta";
-import Section from "@/components/Section";
-import styles from "@/styles/page-category.module.scss"
-function category() {
-    const breadcrumbList = [
-        {
-            name: "ホーム",
-            path: "/",
-        },
-        {
-            name: "カテゴリー",
-            path: "/category",
-        },
-    ]
-    return (
-        <>
-            <Meta
-                title="カテゴリー"
-            />
-            <div className={styles.wrap}>
-                <Breadcrumb list={breadcrumbList}></Breadcrumb>
-                <Section width="small">
-                    <Categories></Categories>
-                </Section>
-            </div>
-        </>
-    );
+import Breadcrumb from '@/components/Breadcrumb'
+import { Categories } from '@/components/Categories'
+import Meta from '@/components/Meta'
+import Section from '@/components/Section'
+import { getAllCategories } from '@/lib/api'
+import styles from '@/styles/page-category.module.scss'
+
+const category = ({ allCategories }) => {
+  console.log(allCategories)
+
+  const breadcrumbList = [
+    {
+      name: 'ホーム',
+      path: '/',
+    },
+    {
+      name: 'カテゴリー',
+      path: '/category',
+    },
+  ]
+  return (
+    <>
+      <Meta title="カテゴリー" />
+      <div className={styles.wrap}>
+        <Breadcrumb list={breadcrumbList}></Breadcrumb>
+        <Section width="small">
+          <Categories allCategories={allCategories} />
+        </Section>
+      </div>
+    </>
+  )
 }
 
-export default category;
+export const getStaticProps = async (ctx) => {
+  const categoriesByDesignType = await getAllCategories('design-type')
+  const categoriesBySiteType = await getAllCategories('site-type')
+  const allCategories = [
+    {
+      catName: 'デザインの系統',
+      catApiName: 'design-type',
+      catSlugs: categoriesByDesignType,
+    },
+    {
+      catName: 'サイトの種類',
+      catApiName: 'site-type',
+      catSlugs: categoriesBySiteType,
+    },
+  ]
+  return {
+    props: {
+      allCategories,
+    },
+  }
+}
+
+export default category
