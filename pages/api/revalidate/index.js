b52d799a4a3ed8eacdc39d591b3cea74c2ae3fff
github takaryu39 <ryuta0takamiya@gmail.com>
2023-09-03 19:46:32 +0900

    投稿の更新に合わせてカテゴリーページも再生性

--- pages/api/revalidate/index.js
+++ pages/api/revalidate/index.js
@@ -5,8 +5,23 @@ const handler = async (req, res) => {
     if (!isCollectSignature(req.headers['x-microcms-signature'], req.body)) {
       return res.status(401).send('Invalid token')
     }
+    const designCategorySlugs = req.body.contents.new.publishValue[
+      'design-type'
+    ].map((item) => item.slug)
+    const siteCategorySlugs = req.body.contents.new.publishValue[
+      'site-type'
+    ].map((item) => item.slug)
 
     await res.revalidate(`/`)
+    await res.revalidate(`/category/design-type`)
+    await res.revalidate(`/category/site-type`)
+    for (const designCategorySlug of designCategorySlugs) {
+      await res.revalidate(`/category/design-type/${designCategorySlug}`)
+    }
+    for (const siteCategorySlug of siteCategorySlugs) {
+      await res.revalidate(`/category/site-type/${siteCategorySlug}`)
+    }
+
     return res.status(200).send()
   } catch (err) {
     return res.status(500).send('Error revalidating')
