2d70b53d09d76e5186299c61c1cfa95f566709fd
github takaryu39 <ryuta0takamiya@gmail.com>
2023-09-03 19:27:27 +0900

    ISRの認証設定

--- pages/api/revalidate/index.js
+++ pages/api/revalidate/index.js
@@ -1,13 +1,12 @@
+import { isCollectSignature } from '@/lib/crypto'
+
 const handler = async (req, res) => {
-  const crypto = require('crypto')
   try {
-    if (!crypto(req.headers['x-microcms-signature'], req.body)) {
+    if (!isCollectSignature(req.headers['x-microcms-signature'], req.body)) {
       return res.status(401).send('Invalid token')
     }
 
-    // const playerId = req.body.contents.new.publishValue.player.id
     await res.revalidate(`/`)
-
     return res.status(200).send()
   } catch (err) {
     return res.status(500).send('Error revalidating')
